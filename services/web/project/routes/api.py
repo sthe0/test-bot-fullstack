263afc890eaf84b3534f6a9e8143f7eb4a45404e
Fedor Solovyev <the0@yandex.ru>
2020-04-16 02:21:01 +0300

    Dummy api.

--- services/web/project/routes/api.py
+++ services/web/project/routes/api.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, request
+from flask import Blueprint, jsonify, request
+from functools import wraps
 from project.common import app, db
 from project.config import ApiConfig
 
@@ -7,8 +8,17 @@ from project.config import ApiConfig
 api = Blueprint('api', __name__)
 
 
+def check_token(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if request.args.get('auth_token') != ApiConfig.AUTH_TOKEN:
+            return jsonify(error='Unauthorized')
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
 @api.route('/bot/api/check')
+@check_token
 def check():
-    if request.args.get('auth_token') != ApiConfig.AUTH_TOKEN:
-        return 'error'
     return 'ok'
